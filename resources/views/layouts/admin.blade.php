c472c3e1dc6cd42cd5951fdc4454c8a365e2e42c
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-06-27 15:39:04 +0200

    aggiunte rotte alla navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,7 +36,7 @@
                 <li><a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
                             class='bx bx-exit'></i> Logout</a></li>
-
+                <!-- da capire cosa fa esattamente -->
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                     @csrf
                 </form>
@@ -46,22 +46,27 @@
 
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i>
-                    <span class="nav_logo-name">Laravel Portfolio</span> </a>
-                <div class="nav_list"> <a href="#" class="nav_link active"> <i
-                            class='bx bx-grid-alt nav_icon'></i>
-                        <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#"
-                        class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span
-                            class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a
-                        href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span
+            <div>
+                <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i><span
+                        class="nav_logo-name">Laravel Portfolio</span> </a>
+                <div class="nav_list">
+                    <a href="{{ url('dashboard') }}" class="nav_link"> <i class='bx bx-grid-alt nav_icon'></i>
+                        <span class="nav_name">Dashboard</span> </a>
+                    <a href="{{ url('profile') }}" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
+                            class="nav_name">Users</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span
+                            class="nav_name">Messages</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span
+                            class="nav_name">Bookmark</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span
                             class="nav_name">Files</span> </a>
                     <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span
                             class="nav_name">Stats</span> </a>
                 </div>
-            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i>
-                <span class="nav_name">SignOut</span> </a>
+            </div>
+            <a href="{{ route('logout') }}"" class="nav_link"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
+                    class='bx bx-log-out nav_icon'></i> Logout</a>
         </nav>
     </div>
 
