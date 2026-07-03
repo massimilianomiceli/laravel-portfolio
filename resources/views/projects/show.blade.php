9e321d28f7905750c8383d96bd7c360054784e5f
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-07-03 19:54:50 +0200

    implementata rotta destroy con aggiunta di modale in pagina

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -93,8 +93,51 @@
                     <a href="{{ route('projects.edit', $project) }}" class="btn btn-edit-custom">
                         Edit
                     </a>
-                </div>
+                    <!-- Pulsante apertura modale -->
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Delete project
+                    </button>
 
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">
+                                        Delete project
+                                    </h5>
+
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                    </button>
+                                </div>
+
+                                <div class="modal-body">
+                                    Are you sure you want to delete
+                                    <strong>{{ $project->name }}</strong>?
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                        Cancel
+                                    </button>
+
+                                    <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
